fa9c7052d64f728426baf4901147546251a920d4
Bamireid123 <142700086+Bamireid123@users.noreply.github.com>
2023-08-20 02:01:20 +0100

    Update settings.js12

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.owner = ['2349160276333'] //ur owner number
+global.ownernomer = "2349160276333" //ur owner number2
+global.ownername = "۝⃟̥̩̩̩̥̩̥͚̮💍࿇𝔹𝕒𝕞𝕚𝕣𝕖𝕚𝕕᳀🌹⃝⃘̉̉̉̉̉̉🧚" //ur owner name
+global.ytname = "YT: ۝⃟̥̩̩̩̥̩̥͚̮💍࿇𝔹𝕒𝕞𝕚𝕣𝕖𝕚𝕕᳀🌹⃝⃘̉̉̉̉̉̉🧚" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Nigeria, Akwa ibom, uyo" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "Bamireids wife"
+global.ownernumber = '2349160276333'
+global.ownername = '۝⃟̥̩̩̩̥̩̥͚̮💍࿇𝔹𝕒𝕞𝕚𝕣𝕖𝕚𝕕᳀🌹⃝⃘̉̉̉̉̉̉🧚'
+global.ownerNumber = ["2349160276333@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = " sticker by #3!$ B∆M!R3!D"
+global.author = "۝⃟̥̩̩̩̥̩̥͚̮💍࿇𝔹𝕒𝕞𝕚𝕣𝕖𝕚𝕕᳀🌹⃝⃘̉̉̉̉̉̉🧚\n\n"
+global.creator = "2349160276333@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go!', 
+    success: 'Here you go!✨', 
 }
 
 let file = require.resolve(__filename)
